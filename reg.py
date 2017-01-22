9a2b70f4fa0e518f0c53ad72764d89b2b8519dc5
nickdrozd <nicholasdrozd@gmail.com>
2017-01-22 11:12:31 -0600

    Add fetch and assign

--- reg.py
+++ reg.py
@@ -1,4 +1,5 @@
-EXP = 'EXP'
+# register shorthand
+EXPR = 'EXPR'
 VAL = 'VAL'
 ENV = 'ENV'
 UNEV = 'UNEV'
@@ -8,3 +9,11 @@ CONT = 'CONT'
 CURR = 'CURR'
 
 STACK = 'STACK'
+
+def fetch(reg):
+	with open(reg, 'r') as regf:
+		return regf.read()
+
+def assign(src, dst):
+	with open(src, 'r') as srcf, open(dst, 'w') as dstf:
+		dstf.write(srcf.read())
