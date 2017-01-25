77109b5f385cfa5a37493df902c094e80a1770d9
nickdrozd <nicholasdrozd@gmail.com>
2017-01-24 18:38:33 -0600

    Add evalIf

--- evalFuncs.py
+++ evalFuncs.py
@@ -26,7 +26,7 @@ def evalDef():
 	save(UNEV)
 	save(ENV)
 	save(CONT)
-	assign(CONT, 'did_def_val')
+	set_continue('did_def_val')
 	goto_eval()
 
 def did_def_val():
@@ -42,4 +42,48 @@ def evalIf():
 	save(EXPR)
 	_, ifTest, _, _ = fetch(EXPR)
 	assign(EXPR, ifTest)
+	set_continue('if_decide')
+	goto_eval()
+
+def if_decide():
+	restore(EXPR)
+	restore(CONT)
+	restore(ENV)
+	if fetch(VAL): # or if isTrue(fetch(VAL))
+		goto('if_then')
+	else:
+		goto('if_else')
+
+def if_then():
+	_, _, ifThen, _ = fetch(EXPR)
+	assign(EXPR, ifThen)
+	goto_eval()	
+
+def if_else():
+	_, _, _, ifElse = fetch(EXPR)
+	assign(EXPR, ifElse)
+	goto_eval()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
