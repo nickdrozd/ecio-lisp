9e4b1385cfceb7892c84610c2e840bcca00d8a46
nickdrozd <nicholasdrozd@gmail.com>
2017-01-27 09:51:44 -0600

    Fix stack restore

--- stack.py
+++ stack.py
@@ -2,16 +2,21 @@ from reg import *
 
 STACK = 'STACK'
 
-def save(reg):
-	with open(STACK, 'a') as stackf:
-		with open(reg, 'r') as regf:
-			stackf.write(str(regf.read()) + '\n')
+# stack taken as arg for testing
 
-def restore(reg):
-	with open(STACK, 'r+') as stackf:
+def save(reg, stack=STACK):
+	with open(stack, 'r+') as stackf:
+		with open(reg, 'r') as regf:
+			reg_contents = str(regf.read())
+			stack_contents = str(stackf.read())
+			joiner = '' if stack_contents == '' else '\n'
+			stackf.write(joiner + reg_contents)
+
+def restore(reg, stack=STACK):
+	with open(stack, 'r+') as stackf:
 		with open(reg, 'w') as regf:
 			*tail, head = stackf.read().split('\n')
-			regf.write(head)
 			tail = '\n'.join(tail)
+			regf.write(head)
 			stackf.seek(len(tail))
 			stackf.truncate()
