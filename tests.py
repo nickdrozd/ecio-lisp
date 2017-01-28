86e176a525413480801ef1783f94c5a18e0dd634
nickdrozd <nicholasdrozd@gmail.com>
2017-01-28 11:04:31 -0600

    Change tests to work on real registers

--- tests.py
+++ tests.py
@@ -16,69 +16,53 @@ class EvalTest(unittest.TestCase):
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-
 		self.verbose = 1
-
-		files = ('test_expr', 'test_val', 
-				'test_env', 'test_unev', 
-				'test_func', 'test_argl', 
-				'test_cont', 'test_stack')
-
-		(self.expr, self.val, self.env,
-		self.unev, self.func, self.argl, 
-		self.cont, self.stack) = files
-
-		self.files = files
 		
 	def setUp(self):
-		self.touch_files()
+		initialize()
 		self.display()
 
 	def tearDown(self):
-		self.rm_files()
+		initialize()
 		self.display()
 
 	# tests #
 
 	def test_assign_and_fetch(self):
 		'''
-		Assign several words to val.
+		Assign several words to VAL.
 		'''
 		for animal in 'cow', 'sheep', 'pig':
-			self.assign_and_verify(self.val, animal)
+			self.assign_and_verify(VAL, animal)
 
 
 	def test_assign_one_reg_to_another(self):
 		'''
-		Assign distinct data to val and cont, 
-		then assign cont to val
+		Assign distinct data to VAL and CONT, 
+		then assign CONT to VAL
 		'''
-		val, cont = self.val, self.cont
+		self.assign_and_verify(VAL, 'lark')
+		self.assign_and_verify(CONT, 'thrush')
 
-
-		self.assign_and_verify(val, 'lark')
-		self.assign_and_verify(cont, 'thrush')
-
-		assign(self.val, fetch(self.cont))
-		self.assert_reg_data(val, 'thrush')
-		self.assert_reg_data(cont, 'thrush')
+		assign(VAL, fetch(CONT))
+		self.assert_reg_data(VAL, 'thrush')
+		self.assert_reg_data(CONT, 'thrush')
 
 
 	def test_save_and_restore(self):
-		val, cont = self.val, self.cont
 
-		self.assign_and_verify(val, 'cat')
-		self.assign_and_verify(cont, 'dog')
+		self.assign_and_verify(VAL, 'cat')
+		self.assign_and_verify(CONT, 'dog')
 
 		self.assert_empty_stack()
 
-		self.save_and_verify(val)
-		self.save_and_verify(cont)
+		self.save_and_verify(VAL)
+		self.save_and_verify(CONT)
 
 		self.assert_stack_depth(2)
 
-		self.restore_and_verify(val)
-		self.restore_and_verify(cont)
+		self.restore_and_verify(VAL)
+		self.restore_and_verify(CONT)
 
 
 	# assertions #
@@ -86,7 +70,7 @@ class EvalTest(unittest.TestCase):
 	def save_and_verify(self, reg):
 		self.display('saving from {}...'.format(reg))
 		depth = self.get_stack_depth()
-		save(reg, self.stack)
+		save(reg)
 		self.show_stack()
 		self.assert_stack_depth(depth + 1)
 		self.assert_stack_top(fetch(reg))
@@ -95,7 +79,7 @@ class EvalTest(unittest.TestCase):
 		self.display('restoring to {}...'.format(reg))
 		depth = self.get_stack_depth()
 		top = self.get_stack_top()
-		restore(reg, self.stack)
+		restore(reg)
 		self.assert_reg_data(reg, top)
 		self.show_stack()
 		self.assert_stack_depth(depth - 1)
@@ -113,7 +97,7 @@ class EvalTest(unittest.TestCase):
 		self.assertEqual(top, data)
 
 	def assert_empty_stack(self):
-		with open(self.stack, 'r') as stack:
+		with open(STACK, 'r') as stack:
 			self.assertEqual(stack.read(), '')
 
 	def assign_and_verify(self, reg, data):
@@ -130,23 +114,25 @@ class EvalTest(unittest.TestCase):
 	# utilities #
 
 	def get_stack_depth(self):
-		with open(self.stack, 'r') as stack:
+		with open(STACK, 'r') as stack:
 			return len(stack.readlines())
 
 	def get_stack_top(self):
-		with open(self.stack, 'r') as stack:
+		with open(STACK, 'r') as stack:
 			data = stack.read()
 			self.assertNotEqual(data, '')
 			return data.split('\n')[-1]			
 
 	def show_stack(self):
-		with open(self.stack, 'r') as stack:
+		with open(STACK, 'r') as stack:
 			stack_contents = stack.read().split('\n')
 			self.display('STACK: ' + str(stack_contents))
 
 	def display(self, msg=''):
 		if self.verbose:
 			print(msg)
+
+
 
 	def touch_files(self):
 		self.run_cmd_on_files('touch')
@@ -158,6 +144,11 @@ class EvalTest(unittest.TestCase):
 		for file in self.files:
 			subprocess.call(
 				'{} {}'.format(cmd, file).split(' '))
+
+
+def initialize():
+	clear_registers()
+	clear_stack()
 
 
 if __name__ == '__main__':
