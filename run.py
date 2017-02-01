cf7f64c8be5242bd04c687218bc8c412154449d9
nickdrozd <nicholasdrozd@gmail.com>
2017-01-31 21:26:22 -0600

    Add repl

--- run.py
+++ run.py
@@ -5,14 +5,25 @@ from instr import *
 from parse import parse
 from evalExp import evalExp
 
+INTERPRETER_PROMPT = '<<< '
+INTERPRETER_EXIT = '.quit'
+
 def get_expr():
-	expr = parse(input())
-	assign(EXPR, expr)
+	expr = parse(input(INTERPRETER_PROMPT))
+
+	if expr == INTERPRETER_EXIT:
+		raise Exception
+	else:
+		assign(EXPR, expr)
+
+
+def get_result():
+	return fetch(VAL)
 
 
 def initialize():
-	clear_registers()
-	clear_stack()
+	# clear_registers()
+	# clear_stack()
 
 	initialize_env()
 	initialize_cont()
@@ -21,9 +32,27 @@ def initialize():
 def run():
 	initialize()
 	get_expr()
-	evalExp()
+
+	# while INSTR != DONE:
+	# 	execute()
+
+	evalExp() # temporary
+
+
+def repl():
+	while True:
+		try:
+			run()
+			print(get_result())
+		except KeyboardInterrupt:
+			print()
+			break
+		except:
+			break
 
 
 def step():
 	"don't run the whole thing, just one step"
 
+if __name__ == '__main__':
+	repl()
