e78eb8629cc0f38b3f42d110cc72409621ec834f
nickdrozd <nicholasdrozd@gmail.com>
2017-03-12 14:31:21 -0500

    Consolidate tests

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 from repl import ecio_eval
-from parse import parse
+# from parse import parse
 
 from garbage import collect_garbage
 from mem import load_memory, write_memory, ROOT
@@ -14,159 +14,107 @@ class EcioTestCase(unittest.TestCase):
 
 
 class TestRun(EcioTestCase):
-    '''Evaluates an expression using all basic language features
-
-    The expression includes a tree-recursive fibonacci function,
-    so it runs pretty slow. Be patient!
-    '''
     def test_run(self):
-        expr = '''
-            (def x 2)
-            (def y 3)
-            (def z 4)
-            (set! z (+ z x y))
-            (def fibonacci
-              (λ (n)
-                (if (< n 2)
-                    n
-                    (+ (fibonacci (- n 1))
-                        (fibonacci (- n 2))))))
-            (def fibz (fibonacci z))
-            (set! x
-              (* (square 3)
-                  fibz))
-            (def addabc
-              (λ (a)
-                (λ (b)
-                  (λ (c)
-                    (λ (x)
-                      (+ x a b c))))))
-            (def d8 (λ () 8))
-            (def result ((((addabc x) fibz) z) (d8)))
-            result
+        self.expr = '''
+          (def x 2)
+          (def y 3)
+          (def z 4)
+          (inc-n! z (_+ x y))
+          (def fibonacci
+            (λ (n)
+              (if (< n 2)
+                  n
+                  (_+ (fibonacci (- n 1))
+                      (fibonacci (- n 2))))))
+          (def fibz (fibonacci z))
+          (set! x
+            (* 1 (square 3) fibz 1))
+          (def addabc
+            (λ (a)
+              (λ (b)
+                (λ (c)
+                  (λ (x)
+                    (+ x a b c))))))
+          (def d8 (λ () 8))
+          (def result ((((addabc x) fibz) z) (d8)))
+          result
         '''
         # z = 9
         # fibz = 34
         # x = 306
         # result = 357
 
-        # test some repl functions too!
-        self.load_and_run(expr)
-
         self.assert_result(357)
 
     def test_loop(self):
-        expr = '''
-            (def count 0)
-            (def loop
-              (λ ()
-                (if (= count 10)
-                    (* count count)
-                    (begin
-                      (set! count (+ count 1))
-                      (loop)))))
-            (loop)
+        self.expr = '''
+          (def total 0)
+          (def count 0)
+          (def loop
+            (λ ()
+              (if (= count 10)
+                  (_* total count)
+                  (begin
+                    (inc-n! count 2)
+                    (inc-n! total (square count))
+                    (loop)))))
+          (loop)
         '''
 
-        self.load_and_run(expr)
-
-        self.assert_result(100)
+        self.assert_result(2200)
 
     def test_redefinition(self):
-        expr = '''
-            (def result 0)
-            (def x 1)
-            (set! result (+ result x))
-            (def x 2)
-            (set! result (+ result x))
-            (set! x 3)
-            (set! result (+ result x))
-            (def f
-              (λ ()
-                (def x 4)
-                (set! result (+ result x))
-                (set! x 5)
-                (set! result (+ result x))))
-            (f)
-            (def g
-              (λ ()
-                (def x 6)
-                (def h
-                  (λ ()
-                    (set! x (+ x 1))
-                    (set! result (+ result x))))
-                (set! result (+ result x))
-                (h)))
-            (g)
+        # macros don't work in function calls?
+        self.expr = '''
+          (def result 0)
+          (def x 1)
+          (inc-n! result x)
+          (def x 2)
+          (inc-n! result x)
+          (set! x 3)
+          (inc-n! result x)
+          (def f
+            (λ ()
+              (def x 4)
+              (set! result (+ result x))
+              (set! x 5)
+              (set! result (+ result x))))
+          (f)
+          (def g
+            (λ ()
+              (def x 6)
+              (def h
+                (λ ()
+                  (set! x (+ x 1))
+                  (set! result (+ result x))))
+              (set! result (+ result x))
+              (h)))
+          (g)
         '''
-
-        self.load_and_run(expr)
 
         self.assert_result(28)
-
-    def test_list_primitives(self):
-        expr = '''
-            (def a (quote (1 2 3)))
-            (def b (cons (car a)
-                         (cdr (cdr a))))
-            (def c (cons a b))
-            (car (cdr c))
-        '''
-
-        self.load_and_run(expr)
-
-        self.assert_result(1)
-
-
-    def test_quasiquote(self):
-        expr = '''
-          (def a 3)
-          (def b 4)
-          (def c 5)
-          (def d 6)
-          (quasiquote
-            ((* a a a)
-              (unq (* a b c))
-              (+ a c d)
-              d
-              (unq d)))
-        '''
-
-        self.load_and_run(expr)
-
-        self.assert_result(parse(
-            '((* a a a) 60 (+ a c d) d 6)'))
-
-    def test_macro(self):
-        expr = '''
-          (def x 5)
-          (defmac 0! (var) (qsq (set! (unq var) 0)))
-          (defmac 1! (var) (qsq (set! (unq var) 1)))
-          (def y (_+ x 2))
-          (0! x)
-          (def z (_+ y x))
-          (1! y)
-          (set! z (_+ z y))
-          z
-        '''
-
-        self.load_and_run(expr)
-
-        self.assert_result(8)
 
     #
 
     def assert_result(self, expected):
+        self.eval_expr()
+
         self.assertEqual(
             self.result,
             expected,
             'Wrong result')
 
-    def load_and_run(self, statements):
-        begin_seq = '(begin {})'.format(statements)
+    def eval_expr(self):
+        begin_seq = '''
+          (begin
+            (defmac inc-n! (var n)
+              (qsq (set! (unq var)
+                         (_+ (unq var)
+                             (unq n)))))
+            {})
+        '''.format(self.expr)
+
         self.result = ecio_eval(begin_seq)
-
-
 
 
 class TestGarbageCollector(EcioTestCase):
